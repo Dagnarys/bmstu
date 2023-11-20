d865c32a26baeaf20a64f00965c9155be37f6bb6
dagnarys <super.iard@yandex.ru>
2023-11-20 20:08:52 +0300

    clear er

--- bmstu_lab/serializers.py
+++ bmstu_lab/serializers.py
@@ -1,3 +1,21 @@
-from datetime import datetime
-from django.db import models
-from django.utils import timezone
+
+from rest_framework import serializers
+from .models import *
+
+
+class CitySerializer(serializers.ModelSerializer):
+    class Meta:
+        # Модель, которую мы сериализуем
+        model = City
+        # Поля, которые мы сериализуем (Все поля)
+        fields = '__all__'
+
+
+class VacancySerializer(serializers.ModelSerializer):
+    cities = CitySerializer(read_only=True, many=True)
+
+    class Meta:
+        # Модель, которую мы сериализуем
+        model = Vacancy
+        # Поля, которые мы сериализуем (Все поля)
+        fields = '__all__

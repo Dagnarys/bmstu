7992f928bfeab166c9d2b1a3a017ad71f2dea5e2
dagnarys <super.iard@yandex.ru>
2023-12-05 17:23:04 +0300

    started 5 lab

--- bmstu/urls.py
+++ bmstu/urls.py
@@ -20,7 +20,7 @@ schema_view = get_schema_view(
 )
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('bmstu_lab.urls')),
+    path('', include('insurances.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui')
 
 ]

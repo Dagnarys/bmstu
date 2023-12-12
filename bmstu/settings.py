6505e024400462fe87da77b68c6309b3d42ea128
dagnarys <super.iard@yandex.ru>
2023-12-12 17:33:38 +0300

    1

--- bmstu/settings.py
+++ bmstu/settings.py
@@ -33,7 +33,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     APP_NAME,
-    'bmstu',
+
     'corsheaders',
     'drf_yasg',
 ]
--- insurances/management/commands/fill_db.py
+++ insurances/management/commands/fill_db.py
@@ -19,71 +19,60 @@ def add_drivers():
         passport_number= '2345 678901'
     ).save()
 
-    Driver(
-
-    ).save()
-    Driver(
-
-    ).save()
-    Driver(
-
-    ).save()
-    Driver(
-
-    ).save()
-
-    full_name: 'Иванов Иван Иванович',
-    birth_date: '1980-05-15',
-    address: 'ул. Примерная, д. 123, кв. 45',
-    phone_number: '+7 (123) 456-7890',
-    email: 'ivan@example.com',
-    driver_license_number: 'AB123456',
-    issue_date: '2005-07-20',
-    expiration_date: '2030-07-20',
-    passport_number: '1234 567890',
+    # Driver(
+    #     full_name: 'Иванов Иван Иванович',
+    #     birth_date: '1980-05-15',
+    #     address: 'ул. Примерная, д. 123, кв. 45',
+    #     phone_number: '+7 (123) 456-7890',
+    #     email: 'ivan@example.com',
+    #     driver_license_number: 'AB123456',
+    #     issue_date: '2005-07-20',
+    #     expiration_date: '2030-07-20',
+    #     passport_number: '1234 567890'
+    # ).save()
 
 
-
-    full_name: 'Петрова Петра Петровна',
-    birth_date: '1995-02-28',
-    address: 'ул. Образцовая, д. 45, кв. 12',
-    phone_number: '+7 (987) 654-3210',
-    email: 'petra@example.com',
-    driver_license_number: 'CD789012',
-    issue_date: '2018-11-10',
-    expiration_date: '2028-11-10',
-    passport_number: '5678 901234',
-    status: 1,
-    image: "",
-    },
-    {
-    id: 4,
-    full_name: 'Сидоров Алексей Васильевич',
-    birth_date: '1988-08-10',
-    address: 'пр. Приморский, д. 56, кв. 3',
-    phone_number: '+7 (777) 123-4567',
-    email: 'alex@example.com',
-    driver_license_number: 'EF345678',
-    issue_date: '2010-03-05',
-    expiration_date: '2030-03-05',
-    passport_number: '9012 345678',
-    status: 1,
-    image: "",
-    },
-    {
-    id: 5,
-    full_name: 'Григорьев Дмитрий Николаевич',
-    birth_date: '1975-12-20',
-    address: 'пр. Ленина, д. 34, кв. 7',
-    phone_number: '+7 (111) 987-6543',
-    email: 'dmitry@example.com',
-    driver_license_number: 'IJ123456',
-    issue_date: '2002-06-12',
-    expiration_date: '2032-06-12',
-    passport_number: '6789 012345',
-    status: 1,
-    image: "",
-
+    #
+    # full_name: 'Петрова Петра Петровна',
+    # birth_date: '1995-02-28',
+    # address: 'ул. Образцовая, д. 45, кв. 12',
+    # phone_number: '+7 (987) 654-3210',
+    # email: 'petra@example.com',
+    # driver_license_number: 'CD789012',
+    # issue_date: '2018-11-10',
+    # expiration_date: '2028-11-10',
+    # passport_number: '5678 901234',
+    # status: 1,
+    # image: "",
+    # },
+    # {
+    # id: 4,
+    # full_name: 'Сидоров Алексей Васильевич',
+    # birth_date: '1988-08-10',
+    # address: 'пр. Приморский, д. 56, кв. 3',
+    # phone_number: '+7 (777) 123-4567',
+    # email: 'alex@example.com',
+    # driver_license_number: 'EF345678',
+    # issue_date: '2010-03-05',
+    # expiration_date: '2030-03-05',
+    # passport_number: '9012 345678',
+    # status: 1,
+    # image: "",
+    # },
+    # {
+    # id: 5,
+    # full_name: 'Григорьев Дмитрий Николаевич',
+    # birth_date: '1975-12-20',
+    # address: 'пр. Ленина, д. 34, кв. 7',
+    # phone_number: '+7 (111) 987-6543',
+    # email: 'dmitry@example.com',
+    # driver_license_number: 'IJ123456',
+    # issue_date: '2002-06-12',
+    # expiration_date: '2032-06-12',
+    # passport_number: '6789 012345',
+    # status: 1,
+    # image: "",
+    #
 
 
 
